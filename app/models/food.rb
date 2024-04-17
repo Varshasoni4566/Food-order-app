d4eddc09b2c059cea615a69dbe35bec429a77de0
Varsha Soni <varsha.soni@poolstack.in>
2024-04-17 18:30:31 +0530

    admin

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,14 +3,14 @@ belongs_to :category
 has_many :line_items
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "id_value", "name", "price","category", "line_items", "updated_at"]
+    ["created_at", "id", "id_value", "name", "price", "updated_at"]
   end
 
   def self.ransackable_associations(auth_object = nil)
     ["category", "line_items"]
   end
 
-  
+
 
   def self.search(query)
     where("name LIKE ?", "%#{query}%")
