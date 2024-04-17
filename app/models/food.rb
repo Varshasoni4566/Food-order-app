3033ee941e81455efa2c9a09e59394d4ca229270
Varsha Soni <varsha.soni@poolstack.in>
2024-04-17 18:25:32 +0530

    admin login

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,11 +1,16 @@
 class Food < ApplicationRecord
-	
 belongs_to :category
 has_many :line_items
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "id_value", "name", "price", "updated_at"]
+    ["created_at", "id", "id_value", "name", "price","category", "line_items", "updated_at"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category", "line_items"]
+  end
+
+  
 
   def self.search(query)
     where("name LIKE ?", "%#{query}%")
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -7,4 +7,8 @@ class LineItem < ApplicationRecord
      return 0 if quantity.nil? || food.nil? || food.price.nil?
      quantity * food.price.to_i
   end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["cart_id", "created_at", "food_id", "id", "id_value", "order_id", "quantity", "updated_at"]
+  end
 end
