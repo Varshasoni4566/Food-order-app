6c7220bcfedc8e3cf30dc5131def88fedc66402f
Varsha Soni <varsha.soni@poolstack.in>
2024-05-01 16:02:25 +0530

    change in reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,11 @@
 class ReportsController < ApplicationController
 
 	def download_csv
-		debugger
 	    report = generate_report(params[:id])
 	    send_data report, filename: "report_#{params[:id]}.csv" 
     end
 
     def download_pdf
-    	debugger
 	    report_data = generate_report_data
 	    send_data generate_pdf(report_data), filename: "report.pdf", type: "application/pdf"
     end
@@ -16,7 +14,6 @@ class ReportsController < ApplicationController
     private
 
 	def generate_report(id)
-		debugger
 	    users = User.all 
 		CSV.generate(headers: true) do |csv|
 		    csv << ['ID', 'Name', 'USERNAME', 'Email', 'PASSWORD'] 
@@ -27,7 +24,6 @@ class ReportsController < ApplicationController
     end
 
     def generate_pdf(report_data)
-    	debugger
 	    pdf = Prawn::Document.new
 	    pdf.text "Report", align: :center, size: 20
 	    pdf.move_down 20
@@ -36,7 +32,6 @@ class ReportsController < ApplicationController
     end
 
     def generate_report_data
-    	debugger
 		users = User.all
 		report_data = []
 		report_data << ["ID", "Name", "Usename", "Email", "Password"] 
